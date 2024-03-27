d39dd3b80c39515bf98efbaedd6b2ab223fce5a4
wiut00013374 <Hp@LAPTOP-1N1SQG72>
2024-03-27 08:43:26 +0500

    slightly improved version

---
+++ SQLQuery1.sql
@@ -0,0 +1,5 @@
+﻿DECLARE @ProductID INT = 1; -- Declare the @ProductID parameter and set its value
+
+SELECT @ProductID AS ProductID, ProductName, ProductPrice
+FROM dbo.Product
+WHERE ProductID = @ProductID; -- Use the @ProductID parameter in your WHERE clause
